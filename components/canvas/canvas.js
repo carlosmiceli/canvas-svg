3372a64673aa64a59e8565418b641d5f60493871
carlosmiceli <carlosmiceli87@gmail.com>
2021-07-17 22:55:27 -0300

    added transformations and data persistance in local storage

--- components/canvas/canvas.js
+++ components/canvas/canvas.js
@@ -1,74 +1,126 @@
-import { Stage, Layer, Path, Transformer } from 'react-konva';
-import { useState } from 'react';
-import axios from 'axios'
-import { parse } from 'svgson'
-import styles from '../../styles/Canvas.module.css'
-
+import { Stage, Layer, Path, Group, Transformer } from "react-konva";
+import { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import { parse } from "svgson";
+import styles from "../../styles/Canvas.module.css";
 
 export default function CanvasComponent(props) {
-    const [svg, setSvg] = useState(null)
-    const [activateDrop, setActivateDrop] = useState(false)
+  const [svg, setSvg] = useState(null);
+  const [activateDrop, setActivateDrop] = useState(false);
+  const [stageHeight, setStageHeight] = useState(null);
+  const [stageWidth, setStageWidth] = useState(null);
+  const [isSelected, setIsSelected] = useState(false);
+  const [svgValues, setSvgValues] = useState(null);
 
-    const handleDragOver = (e) => {
-        e.preventDefault()
+  const stageCanvasRef = useRef(null);
+  const svgRef = useRef(null);
+  const transformerRef = useRef(null);
+
+  useEffect(() => {
+    setStageHeight(stageCanvasRef.current.getBoundingClientRect().height);
+    setStageWidth(stageCanvasRef.current.getBoundingClientRect().width);
+  }, []);
+
+  useEffect(() => {
+    if (svg) {
+      localStorage.setItem("svg-file", JSON.stringify(svg));
     }
-
-    const handleDrop = () => {
-        setActivateDrop(true)
-        axios.get(props.svgFile)
-        .then(res => parse(res.data))
-        .then(data => {
-            setSvg(data)
-        })
+    let oldSvg = JSON.parse(localStorage.getItem("svg-file"));
+    if (oldSvg && !svg) {
+      setSvg(oldSvg);
+      setActivateDrop(true);
     }
+    let newSvgValues = JSON.parse(localStorage.getItem("new-svg-values"));
+    if (newSvgValues) {
+      setSvgValues(newSvgValues);
+    }
+  }, [svg]);
 
-    
+  const handleTransform = () => {
+    let newSvgValues = {
+      x: svgRef.current.attrs.x,
+      y: svgRef.current.attrs.y,
+      scaleX: svgRef.current.attrs.scaleX,
+      scaleY: svgRef.current.attrs.scaleY,
+      rotation: svgRef.current.attrs.rotation,
+    };
+    setSvgValues(newSvgValues);
+    localStorage.setItem("new-svg-values", JSON.stringify(newSvgValues));
+  };
 
-    // const generateShapes = () => {
-        //     return [...Array(10)].map((_, i) => ({
-        //         id: i.toString(),
-        //         x: Math.random() * window.innerWidth,
-        //         y: Math.random() * window.innerHeight,
-        //         rotation: Math.random() * 180,
-        //         isDragging: false,
-        //     }));
-        // }
+  const handleDragOver = e => {
+    e.preventDefault();
+  };
 
-        // const someHandler = () => {
-        //     // Access reference value:
-        //     const value = reference.current;
-        //     const newValue = 1
-        //     // Update reference value:
-        //     reference.current = newValue;
-        //   };
+  const handleDrop = () => {
+    setIsSelected(false);
+    if (svg) {
+      setSvg(null);
+    }
+    if (!activateDrop) {
+      setActivateDrop(true);
+    }
+    axios
+      .get(props.svgFile)
+      .then(res => parse(res.data))
+      .then(data => {
+        setSvg(data);
+      });
+    handleTransform();
+  };
 
-    return (
-        <div className={styles.canvas} onDragOver={handleDragOver} onDrop={handleDrop}>
-            {activateDrop && svg
-                    ?
-            <Stage height={500} width={500} draggable>       
-                <Layer>
-                    {
-                    svg.children.map(s => {
-                        return(
-                            <Path 
-                                data={s.attributes.d}
-                                stroke={s.attributes.stroke}
-                                strokeWidth={s.attributes['stroke-width']}
-                                fill={s.attributes.fill}
-                                key={Math.random()}
-                            />                    
-                        )
-                        })
-                    }
-                </Layer>
-            </Stage>  
-            :
-            null
-            }     
-        </div>
-    )
+  const checkDeselect = e => {
+    const clickedOnEmpty = e.target === e.target.getStage();
+    if (clickedOnEmpty) {
+      setIsSelected(false);
+    }
+  };
+
+  return (
+    <div
+      className={styles.canvas}
+      onDragOver={handleDragOver}
+      onDrop={handleDrop}
+      ref={stageCanvasRef}
+    >
+      <Stage
+        height={stageHeight}
+        width={stageWidth}
+        onMouseDown={checkDeselect}
+      >
+        <Layer>
+          {activateDrop && svg ? (
+            <Group
+              ref={svgRef}
+              onClick={() => setIsSelected(true)}
+              draggable
+              onDragEnd={handleTransform}
+              onDragMove={handleTransform}
+              onTransformEnd={handleTransform}
+              x={svgValues.x}
+              y={svgValues.y}
+              rotation={svgValues.rotation}
+              scaleX={svgValues.scaleX}
+              scaleY={svgValues.scaleY}
+            >
+              {svg.children.map(s => {
+                return (
+                  <Path
+                    data={s.attributes.d}
+                    stroke={s.attributes.stroke}
+                    strokeWidth={Number(s.attributes["stroke-width"]) || 0}
+                    fill={s.attributes.fill}
+                    key={s.attributes["data-ds-id"]}
+                  />
+                );
+              })}
+            </Group>
+          ) : null}
+          {activateDrop && svg && isSelected ? (
+            <Transformer ref={transformerRef} nodes={[svgRef.current]} />
+          ) : null}
+        </Layer>
+      </Stage>
+    </div>
+  );
 }
-
-    
-
--- components/svgs/svgs.js
+++ components/svgs/svgs.js
@@ -1,58 +1,33 @@
 // import { useState, useEffect, useRef } from 'react';
-import Image from 'next/image'
-import styles from '../../styles/Canvas.module.css'
-import Svg1 from '../../images/svg1.svg'
-import Svg2 from '../../images/svg2.svg'
-import Svg3 from '../../images/svg3.svg'
+import Image from "next/image";
+import styles from "../../styles/Canvas.module.css";
+import Svg1 from "../../images/svg1.svg";
+import Svg2 from "../../images/svg2.svg";
+import Svg3 from "../../images/svg3.svg";
 
 export default function Svgs(props) {
+  const startDragging = ev => {
+    let dragData = ev.target.src.replace("http://localhost:3000", "");
+    props.setBeginDraggingSvg(dragData);
+  };
 
-    const startDragging = (ev) => {
-        let dragData = ev.target.src.replace("http://localhost:3000", "");
-        props.setBeginDraggingSvg(dragData)
-    }
-
-    return (
-        <div className={styles.svgList}>
-            {"outdoors".includes(props.category) || !props.category
-            ?
-            <div className={styles.svg}>
-            <Image 
-                onDragStart={startDragging}
-                draggable
-                src={Svg1} 
-                alt="svg1"
-            />
-            </div>
-            :
-            null
-            }
-            {"people".includes(props.category) || !props.category
-            ?
-            <div className={styles.svg}>
-            <Image 
-                onDragStart={startDragging}
-                draggable
-                src={Svg2} 
-                alt="svg2"
-            />
-            </div>
-            :
-            null
-            }
-            {"animals".includes(props.category) || !props.category
-            ?
-            <div className={styles.svg}>
-            <Image 
-                onDragStart={startDragging}
-                draggable
-                src={Svg3} 
-                alt="svg3"
-            />
-            </div>
-            :
-            null
-            }
+  return (
+    <div className={styles.svgList}>
+      {"outdoors".includes(props.category) || !props.category ? (
+        <div className={styles.svg}>
+          <Image onDragStart={startDragging} draggable src={Svg1} alt="svg1" />
         </div>
-    )
+      ) : null}
+      {"people".includes(props.category) || !props.category ? (
+        <div className={styles.svg}>
+          <Image onDragStart={startDragging} draggable src={Svg2} alt="svg2" />
+        </div>
+      ) : null}
+      {"animals".includes(props.category) || !props.category ? (
+        <div className={styles.svg}>
+          <Image onDragStart={startDragging} draggable src={Svg3} alt="svg3" />
+        </div>
+      ) : null}
+    </div>
+  );
 }
